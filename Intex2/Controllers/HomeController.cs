80d5d3c0dfc22449588a19418f4af18df6fec93a
kaylajt-1 <kayla.telford@byu.edu>
2022-04-04 13:10:38 -0600

    CrashSummary Page

--- Intex2/Controllers/HomeController.cs
+++ Intex2/Controllers/HomeController.cs
@@ -29,7 +29,8 @@ namespace Intex2.Controllers
         [HttpGet]
         public IActionResult AllCrashes()
         {
-            return View();
+            var crashes = repo.Crashes.ToList();
+            return View("CrashSummary", crashes);
         }
 
 
--- Intex2/Models/CrashContext.cs
+++ Intex2/Models/CrashContext.cs
@@ -1,22 +1,22 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace Intex2.Models
-//{
-//    public class CrashContext : DbContext
-//    {
-//        //Constructor 
-//        public CrashContext(DbContextOptions<CrashContext> options) : base(options)
-//        {
+namespace Intex2.Models
+{
+    public class CrashContext : DbContext
+    {
+        //Constructor 
+        public CrashContext(DbContextOptions<CrashContext> options) : base(options)
+        {
 
-//        }
-//        public DbSet<Crash> Crashes { get; set; }
+        }
+        public DbSet<Crash> Crashes { get; set; }
 
-//    }
-//}
+    }
+}
 
 
 
