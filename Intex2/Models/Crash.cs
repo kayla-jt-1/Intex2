8cdbee9683ecff77eef91da61229523a8bc09ea6
alismith <alimsmith19@gmail.com>
2022-04-04 09:23:28 -0600

    created some new pages

---
+++ Intex2/Models/Crash.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex2.Models
+{
+    public class Crash
+    {
+        [Key]
+        [Required]
+        public int CrashID { get; set; }
+    }
+}
--- Intex2/Startup.cs
+++ Intex2/Startup.cs
@@ -24,6 +24,8 @@ namespace Intex2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //services.AddScoped<ICrashRepository, EFCrashRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
