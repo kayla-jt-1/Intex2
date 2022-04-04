c5737dee8356644ce7efa729a4eb96f99d413068
alismith <alimsmith19@gmail.com>
2022-04-04 09:39:58 -0600

    connecting to mysql?

---
+++ Intex2/Models/CrashDBContext.cs
@@ -0,0 +1,24 @@
+﻿//This file is used to connect our MySQL database 
+
+
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+//namespace Intex2.Models
+//{
+//    public class CrashDBContext : DbContext
+//    {
+//        public CrashDBContext()
+//          : base(GetRDSConnectionString())
+//        {
+//        }
+
+//        public static CrashDBContext Create()
+//        {
+//            return new CrashDBContext();
+//        }
+//    }
+//}
